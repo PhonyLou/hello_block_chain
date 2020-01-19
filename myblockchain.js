b8b7805aa1b4cd4df38afa40d9827ae6f1e0081a
Qilin Lou <qilin.lou@thoughtworks.com>
2020-01-19 15:45:03 +0800

    Add basic blocks

--- myblockchain.js
+++ myblockchain.js
@@ -49,19 +49,37 @@ class BlockChain {
         this.difficulty = 4;
     }
 
+    /**
+     * 创世区块
+     */
     createGenesis() {
         return new Block(0, timestamp, "Genesis block", "0");
     }
 
     getLatestBlock() {
-
+        return this.chain[this.chain.length - 1];
     }
 
     addBlock(newBlock) {
-
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.mineBlock(this.difficulty);
+        this.chain.push(newBlock);
     }
 
     checkValid() {
+        for(let i = 1; i < this.chain.length; i++) {
 
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if (currentBlock.hash != currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if (currentBlock.previousHash != previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
     }
 }
